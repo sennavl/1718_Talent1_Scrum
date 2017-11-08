cf42fc451461fecf612f86ee5d825a9e45fd793f
Senna Van Londersele <senna.vanlondersele@student.odisee.be>
2017-11-08 16:22:05 +0100

    Added java project (lab 2)

---
+++ PokerHands/src/pokerhands/PokerHand.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokerhands;
+
+import static pokerhands.Card.getRandomCard;
+
+/**
+ *
+ * @author senna.vanlondersele
+ */
+public class PokerHand {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+    
+}
