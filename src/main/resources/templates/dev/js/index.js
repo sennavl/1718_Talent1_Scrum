cd79554cbd3fc801d7afc31c793e769b763be0d6
Seppe Snoeck <seppe.snoeck@student.odisee.be>
2017-11-30 10:45:07 +0100

    React-Redux (Voorbeeld zit er nog in)

---
+++ src/main/resources/templates/dev/js/index.js
@@ -0,0 +1,24 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import allReducers from './reducers';
+import App from './components/App';
+
+const store = createStore(
+    allReducers,
+    compose(
+        applyMiddleware(thunk, promise),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
