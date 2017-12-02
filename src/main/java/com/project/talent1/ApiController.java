18ed2790cdddb9ccfdb1b3683cd3aeb6c41c5362
Senna Van Londersele <senna.vanlondersele@student.odisee.be>
2017-12-02 01:24:19 +0100

    Test with local db connection

--- src/main/java/com/project/talent1/ApiController.java
+++ src/main/java/com/project/talent1/ApiController.java
@@ -5,9 +5,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.project.talent1.User;
+import com.project.talent1.UserRepository;
+
 @RequestMapping("/api")
 @RestController
 public class ApiController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(path="/testget")
+    public @ResponseBody Iterable<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
     @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public String index() {
         return "Hello World!";
---
+++ src/main/java/com/project/talent1/UserRepository.java
@@ -0,0 +1,9 @@
+package com.project.talent1;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.project.talent1.User;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+
+}
