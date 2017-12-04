2f95ffa64241c07a7f156f75dcd4f703bb3de4f7
Jan <janrob422@gmail.com>
2017-12-04 15:29:19 +0100

    Register + Login

--- src/main/java/com/project/talent1/ApiController.java
+++ src/main/java/com/project/talent1/ApiController.java
@@ -1,10 +1,16 @@
 package com.project.talent1;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RequestMapping("/api")
 @RestController
@@ -20,6 +26,36 @@ public class ApiController {
     @GetMapping(path = "/users/{id}")
     public @ResponseBody Users getUser(@PathVariable long id){
         return userRepository.findById(id);
+    }
+
+    @RequestMapping(path = "/users/register",method = RequestMethod.POST)
+    public @ResponseBody Users registerUser(@RequestBody Users s,HttpServletResponse response){
+        try {
+            s.setPassword(BCrypt.hashpw(s.getPassword(),BCrypt.gensalt()));
+            return userRepository.save(s);
+        }catch (Exception e){
+            response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+            return null;
+        }
+
+    }
+
+    @RequestMapping(path = "/users/login",method = RequestMethod.POST)
+    public Users login(@RequestBody Users u,HttpServletResponse response) {
+        Users s = userRepository.findByEmail(u.getEmail());
+        try{
+            if(BCrypt.checkpw(u.getPassword(),s.getPassword())){
+                return s;
+            }
+            else{
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                return null;
+            }
+        }catch (NullPointerException e){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return null;
+        }
+
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
--- src/main/java/com/project/talent1/UserRepository.java
+++ src/main/java/com/project/talent1/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<Users, Long>{
     Users findById(long id);
+    Users findByEmail(String email);
 }
