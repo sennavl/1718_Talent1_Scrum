1003d297f19d0e6291f8dccab239e20fb2ae0e89
Jan <janrob422@gmail.com>
2017-12-06 21:19:21 +0100

    Possible fix allowOrigin

--- src/main/java/com/project/talent1/Talent1Application.java
+++ src/main/java/com/project/talent1/Talent1Application.java
@@ -2,9 +2,23 @@ package com.project.talent1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Talent1Application {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Talent1Application.class, args);
