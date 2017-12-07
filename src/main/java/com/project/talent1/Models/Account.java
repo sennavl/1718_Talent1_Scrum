ec425dcd3c4431b4fd462c93a864cb5afd85752c
Jan Robert <jan.robert@student.odisee.be>
2017-12-07 11:07:04 +0100

    DB classes update

--- src/main/java/com/project/talent1/Models/Account.java
+++ src/main/java/com/project/talent1/Models/Account.java
@@ -2,15 +2,21 @@ package com.project.talent1.Models;
 
 import com.project.talent1.Repositories.PersonRepository;
 import com.project.talent1.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Account {
     public int id;
+    public String firstName,lastName;
+    public String email;
+    public java.sql.Date date;
+    public String pass;
 
-
+    @Autowired
     private UserRepository users;
+    @Autowired
     private PersonRepository people;
 
-    public Account(){
+    public Account(Persons p){
 
     }
 }
