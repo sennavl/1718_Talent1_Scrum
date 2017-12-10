6adf60a704e09e1a28ea11d1fad031c047c15f05
Simon <simon.parewyck@student.odisee.be>
2017-12-10 17:27:10 +0100

    Test get top 20

--- src/main/java/com/project/talent1/Repositories/TalentRepository.java
+++ src/main/java/com/project/talent1/Repositories/TalentRepository.java
@@ -1,8 +1,14 @@
 package com.project.talent1.Repositories;
 
 import com.project.talent1.Models.Talents;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface TalentRepository extends CrudRepository<Talents,Long> {
     Talents findById(long id);
+
+    @Query(nativeQuery = true, value = "SELECT TOP 20 * FROM talents ORDER BY matches DESC")
+    List<Talents> findTop20();
 }
