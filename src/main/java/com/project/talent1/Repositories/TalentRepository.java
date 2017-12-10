9697ad9b0ada6a1908391889ee67dde309aea31f
Simon <simon.parewyck@student.odisee.be>
2017-12-10 17:54:05 +0100

    Get all talents query

--- src/main/java/com/project/talent1/Repositories/TalentRepository.java
+++ src/main/java/com/project/talent1/Repositories/TalentRepository.java
@@ -9,6 +9,9 @@ import java.util.List;
 public interface TalentRepository extends CrudRepository<Talents,Long> {
     Talents findById(long id);
 
+    @Query(nativeQuery = true, value = "SELECT * FROM talents ORDER BY matches DESC")
+    List<Talents> findAll();
+
     @Query(nativeQuery = true, value = "SELECT TOP 20 * FROM talents ORDER BY matches DESC")
     List<Talents> findTop20();
 }
