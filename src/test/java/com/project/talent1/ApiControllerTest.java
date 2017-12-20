a37b42fef492a9b0185586ea4ebde4ff3191678c
Senna Van Londersele <senna.vanlondersele@student.odisee.be>
2017-12-20 19:16:25 +0100

    Extended tests

--- src/test/java/com/project/talent1/ApiControllerTest.java
+++ src/test/java/com/project/talent1/ApiControllerTest.java
@@ -117,10 +117,21 @@ public class ApiControllerTest {
     }
 
     @Test
-    public void searchUser() throws Exception {
+    public void searchUserByFirstname() throws Exception {
         mockMvc.perform(get("/api/users/search/Peter"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)));
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].person_id", is(toIntExact(personId2))))
+                .andExpect(jsonPath("$[0].birthday", is("1990-05-04")));
+    }
+
+    @Test
+    public void searchUserByLastname() throws Exception {
+        mockMvc.perform(get("/api/users/search/peeters"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].person_id", is(toIntExact(personId2))))
+                .andExpect(jsonPath("$[0].birthday", is("1990-05-04")));
     }
 
     @Test
